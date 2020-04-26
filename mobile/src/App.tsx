9a1f555be81d289058db761e99e368e71ef053b8
Luan Lima <luanyata@gmail.com>
2020-04-26 18:15:56 -0300

    :tada:

---
+++ mobile/src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react';
+import { View, Text } from 'react-native';
+
+const App: FunctionComponent = () => (
+  <View>
+    <Text>Luan Lima</Text>
+  </View>
+);
+
+export default App;
