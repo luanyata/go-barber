2b974ec3a0590009a67b421c8c306ddac400f68e
Luan Lima <luanyata@gmail.com>
2020-05-13 21:39:32 -0300

    :sparkles: Operacao com senha
    funcionalidade de troca de senha; funcionalidade
    recuperacao de senha

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -5,7 +5,10 @@ import IAppointmentsRepository from '@modules/appointments/repositories/IAppoint
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository'
 import IUsersRepository from '@modules/users/repositories/IUsersRepository'
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepositories'
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository'
+import UsersTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokenRepository'
 
 
 container.registerSingleton<IAppointmentsRepository>('AppointmentsRepository', AppointmentsRepository)
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository)
+container.registerSingleton<IUserTokensRepository>('UserTokensRepository', UsersTokensRepository)
