e7d536ceb71b6d6e5f619a0cd8daca201186f66d
Luan Lima <luanyata@gmail.com>
2020-05-17 17:44:53 -0300

    :heavy_plus_sign: Adição do HanbleBars
    Criação do template para email

--- backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -31,7 +31,20 @@ class SendForgotPasswordEmailService {
 
 		const { token } = await this.userTokensRepository.generate(user.id)
 
-		await this.mailProvider.sendMail(email, `Teste recuperação de senha ${token}`)
+		await this.mailProvider.sendMail({
+			to: {
+				name: user.name,
+				email: user.email
+			},
+			subject: '[GoBarber] Recuperação de Senha',
+			templateData: {
+				template: 'Olá, {{name}} {{token}}',
+				variables: {
+					name: user.name,
+					token
+				}
+			}
+		})
 	}
 }
 
