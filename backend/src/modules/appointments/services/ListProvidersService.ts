5b1ad13c092621ab515ea532387056507ceb3611
Luan Lima <luan@yata.dev>
2020-06-18 17:03:24 -0300

    :sparkles: Cache da lista de Providers
    Criação do cache da lista de providers no redis

--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -1,5 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import User from '@modules/users/infra/typeorm/entities/User';
 
 interface IRequest {
@@ -11,12 +12,26 @@ class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ userId }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders({
-      expectUserId: userId,
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${userId}`,
+    );
+
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        expectUserId: userId,
+      });
+
+      console.log('Query Feita no banco');
+
+      await this.cacheProvider.save<User[]>(`providers-list:${userId}`, users);
+    }
+
     return users;
   }
 }
