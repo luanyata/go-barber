2cd735cb4a3c4d648695aa156b7630c900a835d7
Luan Lima <luan@yata.dev>
2020-06-19 14:15:21 -0300

    :sparkles: Rate Limiter
    Limitação da quantidades de requests por segundo na api

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -7,13 +7,15 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import AppError from '@shared/errors/AppError';
 import { errors } from 'celebrate';
-
 import uploadConfig from '@config/upload';
+import rateLimiter from './middlewares/rateLimeter';
+
 import routes from './routes';
 
 import '@shared/container';
 
 const app = express();
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
